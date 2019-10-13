5c052d2b29431dbc3572d9f613042d3b9d6ea9f3
DavidGarciaGiron <david.garcia@tecsup.edu.pe>
2019-10-12 19:36:56 -0500

    first commit

---
+++ University/Database1/Enrollment.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Enrollment]
+(
+	[EnrollmentID]  INT IDENTITY (1, 1)   NOT NULL,
+	[Grade]         DECIMAL      (3, 2)   NULL,
+	[StudentID]     INT                   NOT NULL,
+	[CourseID]      INT                   NOT NULL,
+	PRIMARY KEY CLUSTERED ([EnrollmentID] ASC),
+	CONSTRAINT [FK_dbo.Enrrollment_dbo.Course_CourseID] FOREIGN KEY ([CourseID])
+		REFERENCES [dbo].[Course] ([CourseID]) ON DELETE CASCADE,
+	CONSTRAINT [FK_dbo.Enrrollment_dbo.Student_StudentID] FOREIGN KEY ([StudentID])
+		REFERENCES [dbo].[Student] ([StudentID]) ON DELETE CASCADE,
+ )
